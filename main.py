91c09bcef0e2ee404537830f64115a413da1fa65
bsartek <lolonouill@gmail.com>
2023-07-25 00:50:42 +0200

    a

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ while running:
         image = pygame.image.load ("assets\IMG_20230310_210204_275.jpg").convert_alpha()
         screen.blit(image, (200,200))
         image = pygame.image.load ("assets\Rn_image_picker_lib_temp_98d895ad-4d84-401e-a8dd-de28c48f54a4.jpg").convert_alpha()
-        image = pygame.transform.scale_by(image, (0.2, 0.1))
+        image = pygame.transform.scale_by(image, (0.1, 0.07))
         screen.blit(image, (250,250))
     pygame.display.flip()
 
