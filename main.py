fa42a9a6e6543e8febd2035d704fed68fe7db872
bsartek <lolonouill@gmail.com>
2023-07-25 13:03:48 +0200

    ad

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h), p
 clock = pygame.time.Clock()
 running = True
 
+music = pygame.mixer.music.load("assets\Fortnite Teamate Down Sound Effect.mp3")
+pygame.mixer.music.play()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,8 +19,8 @@ while running:
         screen.blit(image, (200,200))
         image = pygame.image.load ("assets\Rn_image_picker_lib_temp_98d895ad-4d84-401e-a8dd-de28c48f54a4.jpg").convert_alpha()
         image = pygame.transform.scale_by(image, (0.1, 0.07))
-        music = pygame.mixer.music.load("assets\Fortnite Teamate Down Sound Effect.mp3")
         screen.blit(image, (250,250))
+        
     pygame.display.flip()
 
     clock.tick(60)  
