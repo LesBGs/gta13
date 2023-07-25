159c3d644681752962893013eae413e59313a8f0
bsartek <lolonouill@gmail.com>
2023-07-25 12:31:20 +0200

    a

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ while running:
         screen.blit(image, (200,200))
         image = pygame.image.load ("assets\Rn_image_picker_lib_temp_98d895ad-4d84-401e-a8dd-de28c48f54a4.jpg").convert_alpha()
         image = pygame.transform.scale_by(image, (0.1, 0.07))
+        music = pygame.mixer.music.load("assets\Fortnite Teamate Down Sound Effect.mp3")
         screen.blit(image, (250,250))
     pygame.display.flip()
 
